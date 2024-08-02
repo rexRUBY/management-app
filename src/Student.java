d96c71e90b7089816de2881be4f3e7fdf271467e
MOON <moonjs1018@naver.com>
2024-08-02 20:42:57 +0900

    first commit

---
+++ src/Student.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+public class Student {
+    private int id;
+    private String name;
+    private List<Subject> subject;
+    private String status;
+
+    public void setId() {
+
+    }
+
+    public int getId() {
+        return 0;
+    }
+
+    public void setName() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSubject() {
+
+    }
+
+    public List<Subject> getSubject() {
+        return List.of();
+    }
+
+    public void setStatus() {
+
+    }
+
+    public String getStatus() {
+        return "";
+    }
+
+
+
+}
---
+++ src/Subject.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Subject {
+    private String subjectId;
+    private String subjectName;
+    private Boolean subjectType;
+    private ArrayList score;
+
+    public String getSubjectId() {
+        return "";
+    }
+
+    public String getSubjectName() {
+        return "";
+    }
+
+    public Boolean isEssential() {
+        return null;
+    }
+
+    public String setSubjectId() {
+        return "";
+    }
+
+    public void setSubjectName() {
+
+    }
+
+    public void setSubjectType() {
+
+    }
+
+    public void setScore() {
+
+    }
+
+    public int getScore() {
+        return 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
