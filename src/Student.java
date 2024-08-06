62f740c1f5604af0815beaada4e1bd23b8c6f67c
KUGOM <yaeji0728@naver.com>
2024-08-06 14:49:01 +0900

    suv

--- src/Student.java
+++ src/Student.java
@@ -14,27 +14,35 @@ public class Student {
         int essentialCount = 0;// 필수과목 카운트
         int selectCount = 0;// 선택과목 카운드
         boolean re = true;
-        //필수과목은 Java, 객체지향, Spring, JPA, MySQL -> count1이 3미만일땐 필수과목은 3개 이상 필강이라고 표시 / 5개 이상일땐 들을 수 있는 과목이 더 이상 없다고 표시 후 break
-        //선택과목은 디자인 패턴, Spring Security, Redis, MongoDB -> count2가 2미만일땐 필수과목은 3개 이상 필강이라고 표시 / 이하동문
+        boolean rep = true;
         List<Subject> SubjectStore = SubjectRepository.getSubjectStore();
         Scanner scan = new Scanner(System.in);
         System.out.print("학생이름 : ");
         String name = scan.next();
         System.out.print("수강할 과목 : ");
         while (re) {
-            String subjectName = scan.next();
-            Subject subject = SubjectRepository.getSubjectName(subjectName);
-            Boolean type = subject.getIsEssential();//입력한 과목이 필수인지 선택인지 판별
-            if (type) {
-                essentialCount++;
-            } else if (!type) {
-                selectCount++;
-            }
+            while(rep){
+                String subjectName = scan.next(); //수강할 과목을 입력
+                Subject subject = SubjectRepository.getSubjectName(subjectName);//입력한 과목과 저장된 과목이 같은지 판별
+                Boolean type = subject.getIsEssential();//입력한 과목이 필수인지 선택인지 판별
+                if (type) {
+                    essentialCount++;
+                } else if (!type) {
+                    selectCount++;
+                }
 
-            if (essentialCount < 3) {
-                System.out.print("필수과목은 3개 이상 필강입니다. 필수과목을 더 입력해주세요");
-            } else if (selectCount == 5) {
-                System.out.print("필수과목은 3개 이상 필강입니다. 필수과목을 더 입력해주세요");
+                if(!studentMap.containsKey(subjectName)){
+                    studentMap.put(subject, new ArrayList<Integer>());//과목저장
+                }
+                else{
+                    System.out.print("이미 입력된 강의입니다. 다른 과목을 입력해주세요");
+                }
+
+                if (essentialCount < 3) {
+                    System.out.print("필수과목은 3개 이상 필강입니다. 필수과목을 더 입력해주세요");
+                 } else if (selectCount == 5) {
+                    System.out.print("필수과목은 3개 이상 필강입니다. 필수과목을 더 입력해주세요");
+                }
             }
 
             if (essentialCount >= 3 && selectCount >= 2) { //필수과목이 3개, 선택과목이 2개 이상일때만 exit문구 출력
@@ -45,7 +53,6 @@ public class Student {
                 }
             }
 
-            studentMap.put(subject, new ArrayList<Integer>());//과목저장
             //map.comtains key로 중복처리
 
         }//무한반복 종료
