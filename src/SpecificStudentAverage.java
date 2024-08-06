0aa839aba9a3f53f1d110f4a9febc3dab1f09ca7
박용준 <dydwnsl02@naver.com>
2024-08-06 20:06:54 +0900

    평균 .. 하 복잡하다 복잡해

--- src/SpecificStudentAverage.java
+++ src/SpecificStudentAverage.java
@@ -2,35 +2,41 @@ public class SpecificStudentAverage {
     public static void averageCalculate() {
         int sum = 0;
         int average = 0;
-/*
-        if (List'과목'.student.status().equalsIgnoreCase("Green")){
-            List'과목'.student.score;
-         List    Students.getstatus
 
-         for (int score : List'과목'.student.status()
+        ##############                                                 ##############
 
-         equalsIgnoreCase("Green")
+        if (List '과목'.student.status().equalsIgnoreCase("Green")){
+            List '과목'.student.score;
+            List Students.getstatus
 
-         for(Student temp : list){
-            if (temp.getStatus().equalsIgnoreCase("Green")){
-                sum += temp.getStudentMap.get()
+
+            // 상태 필터링 -> 과목 필터링
+            // 메서드 green인 학생 뽑는 메서드, 과목별로 나누는 메서드 두 가지 이용
+
+            for (Student temp : list) {
+                if (temp.getStatus().equalsIgnoreCase("Green")) {
+                    if () // 과목
+                }
+                sum += temp.getStudentMap.get(Subject)
+
             }
 
-
+##############                                                 ##############
+            for (int i : ScoreRegist.yj) {
+                sum += i;
+            }
+            average = sum / ScoreRegist.yj.size();
+            System.out.println("평균 : " + average);
 
         }
+    }
 
+    public static void pickStatusUp(String status) {
+        for (Student temp : StudentRepository.students) {
+            if (temp.getStatus().equalsIgnoreCase(status)) {
 
-  */
+            }
 
-
-
-        for (int i : ScoreRegist.yj) {
-            sum += i;
         }
-        average = sum / ScoreRegist.yj.size();
-        System.out.println("평균 : " +average);
-
-
     }
 }
