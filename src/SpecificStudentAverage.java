b3ac9769a15c7bee3216d5e7a95049a9c749c33e
박용준 <dydwnsl02@naver.com>
2024-08-06 18:17:04 +0900

    rebase dev

--- src/SpecificStudentAverage.java
+++ src/SpecificStudentAverage.java
@@ -2,6 +2,28 @@ public class SpecificStudentAverage {
     public static void averageCalculate() {
         int sum = 0;
         int average = 0;
+/*
+        if (List'과목'.student.status().equalsIgnoreCase("Green")){
+            List'과목'.student.score;
+         List    Students.getstatus
+
+         for (int score : List'과목'.student.status()
+
+         equalsIgnoreCase("Green")
+
+         for(Student temp : list){
+            if (temp.getStatus().equalsIgnoreCase("Green")){
+                sum += temp.getStudentMap.get()
+            }
+
+
+
+        }
+
+
+  */
+
+
 
         for (int i : ScoreRegist.yj) {
             sum += i;
