809048e53ad1370d2b6e230bfff6f40a5a54e3b8
“cbb715” <iseul1121@gmail.com>
2024-08-06 14:23:31 +0900

    refactor : 메인뷰

--- src/DisplayScoreView.java
+++ src/DisplayScoreView.java
@@ -1,2 +1,29 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class DisplayScoreView {
+    private static Scanner sc = new Scanner(System.in);
+
+    static void displayScoreView() {
+        boolean flag = true;
+        while (flag) {
+            System.out.println("\n====== 점수 관리 ======");
+            System.out.println("1. 점수 등록");
+            System.out.println("2. 점수 수정");
+            System.out.println("3. 점수 조회");
+            System.out.println("4. 이전으로 돌아가기");
+            System.out.println("관리 항복을 선택하세요 : ");
+            int menuInput = sc.nextInt();
+
+            switch (menuInput) {
+                case 1 -> System.out.println("점수 등록");
+                case 2 -> System.out.println("점수 수정");
+                case 3 -> System.out.println("점수 조회");
+                case 4 -> flag = false;
+                default -> {
+                    System.out.println("잘못된 입력입니다! \n처음으로 돌아갑니다.");
+                }
+            }
+        }
+    }
 }
--- src/DisplayStudentView.java
+++ src/DisplayStudentView.java
@@ -1,10 +1,10 @@
-mport java.util.List;
+import java.util.List;
 import java.util.Scanner;
 
 public class DisplayStudentView {
     private static Scanner sc = new Scanner(System.in);
 
-    private static void displayStudentView() {
+    static void displayStudentView() {
         boolean flag = true;
         while (flag) {
             System.out.println("\n====== 수강생 관리 ======");
@@ -15,53 +15,13 @@ public class DisplayStudentView {
             int menuInput = sc.nextInt();
 
             switch (menuInput) {
-                case 1 -> a //수강생 정보등록
-                case 2 -> b //수강생 목록조회
-                case 3 -> displayMainView();
+                case 1 -> System.out.println("수강생 정보등록");
+                case 2 -> System.out.println("수강생 목록조회");
+                case 3 -> flag = false;
                 default -> {
                     System.out.println("잘못된 입력입니다! \n처음으로 돌아갑니다.");
                 }
-
             }
         }
     }
-
-    private static void a() {
-        boolean flag = true;
-        int menuInput = sc.nextInt();
-
-        switch (menuInput) {
-            case 1 -> //고유번호 등록
-            case 2 -> //이름 등록
-            case 3 -> //과목목록 등록
-            case 4 -> //수강생상태 등록
-            default -> {
-                System.out.println("잘못된 입력입니다! \n처음으로 돌아갑니다.");
-            }
-        }
-    }
-}
-
-private static void b() {
-    boolean flag = true;
-    while (flag) {
-        System.out.println("\n====== 수강생 목록 조회 ======");
-        System.out.println("1. 고유번호를 조회하세요");
-        System.out.println("2. 이름을 조회하세요");
-        System.out.println("3. 수강생 상태를 조회하세요");
-        System.out.println("관리 항복을 선택하세요 : ");
-        int menuInput = sc.nextInt();
-
-        switch (menuInput) {
-            case 1 -> //고유번호 조회
-            case 2 -> //이름 조회
-            case 3 -> //수강생상태 조회
-            default -> {
-                System.out.println("잘못된 입력입니다! \n처음으로 돌아갑니다.");
-            }
-
-        }
-    }
-}
-
 }
