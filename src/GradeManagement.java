818e80c0d87448379a3a8837050df9bb2aa901e2
MOON <moonjs1018@naver.com>
2024-08-07 19:34:21 +0900

    add : 특정 학생 과목의 모든 회차별 등급 조회 기능 구현

--- src/GradeManagement.java
+++ src/GradeManagement.java
@@ -6,10 +6,10 @@ public class GradeManagement {
     private static List<Student> studentList = StudentRepository.getStudents();
     Scanner sc = new Scanner(System.in);
 
-    public void gradeView() {
+    public void subjectGradeView() {
         boolean flag = true;
         while(flag) {
-            System.out.println("======= 등급 조회 ======= ");
+            System.out.println("======= 특정 학생 과목별 등급 조회 ======= ");
 
             // 수강생 리스트 출력
             for (Student student : studentList) {
@@ -34,7 +34,7 @@ public class GradeManagement {
                 continue;
             }
 
-            calculateGrade(targetStudent);
+            calculateSubjectGrade(targetStudent);
 
             System.out.println("뒤로가기 (exit)");
             String isExit = sc.next();
@@ -45,7 +45,70 @@ public class GradeManagement {
         }
     }
 
-    public void calculateGrade(Student student) {
+    public void studentRoundGradeView() {
+        boolean flag = true;
+        while(flag) {
+            System.out.println("======= 특정 학생 회차별 등급 조회 ======= ");
+
+            // 수강생 리스트 출력
+            for (Student student : studentList) {
+                StudentRepository.printStudentInfo(student);
+            }
+            System.out.print("등급 조회를 원하는 학생의 고유 번호를 입력하세요 : ");
+            int studentNum = -1;
+
+            try {
+                studentNum = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("유효하지 않은 숫자입니다. ");
+                sc.nextLine();
+                continue;
+            }
+
+            // 입력 받은 학생
+            Student targetStudent = StudentRepository.findStudentById(studentNum);
+
+            if (targetStudent == null) {
+                System.out.println("존재하지 않은 수강생이거나 입력이 잘못되었습니다.");
+                continue;
+            }
+
+            // 과목 선택
+            List<String> checkNameList = new ArrayList<>(); // 존재하는 과목 체크 위한 리스트
+            System.out.println("======== 과목을 선택하세요 ========");
+            for (Subject subject : targetStudent.getStudentMap().keySet()) {
+                String temp = subject.getSubjectName();
+                System.out.println(temp);
+                checkNameList.add(temp);
+            }
+            System.out.println("===============================");
+            String targetSubject = sc.next();
+
+            // 잘못입력 예외 처리
+            if (!checkNameList.contains(targetSubject)) {
+                System.out.println("없는 과목을 입력하였습니다.");
+                break;
+            }
+
+            while (true) {
+                calculateRoundGrade(targetStudent, targetSubject);
+                System.out.println("다른 회차를 조회하시겠습니까? (yes or exit)");
+                String input = sc.next();
+                if (input.equals("exit")) {
+                    break;
+                }
+            }
+
+//            System.out.println("뒤로가기 (exit)");
+//            String isExit = sc.next();
+//
+//            if (isExit.equals("exit")) {
+//                flag = false;
+//            }
+        }
+    }
+
+    public void calculateSubjectGrade(Student student) {
         Map<Subject, ArrayList<Integer>> targetMap = student.getStudentMap();
         Set<Subject> set = targetMap.keySet();
 
@@ -60,8 +123,74 @@ public class GradeManagement {
                 sum += score;
             }
 
-            System.out.println(subject.getSubjectName() + " 의 평균 등급 : " + sum / roundNum);
-        }
+            int result = sum / roundNum;
+            String grade = "";
 
+            if (subject.getIsEssential()) {
+                grade = essentialGrade(result);
+            } else {
+                grade = optianalGrade(result);
+            }
+
+            System.out.println(subject.getSubjectName() + " 평균 등급 : " + grade);
+        }
+    }
+
+    public void calculateRoundGrade(Student student, String targetSubject) {
+        Map<Subject, ArrayList<Integer>> targetMap = student.getStudentMap();
+        Set<Subject> set = targetMap.keySet();
+
+
+        // 과목을 순차적으로 돌면서
+        for (Subject subject : set) {
+            boolean isEssential = subject.getIsEssential(); // 필수인지 선택인지 저장
+            if (subject.getSubjectName().equals(targetSubject)) {
+                int i = 1; // 회차 인덱스// 특정 과목의 점수 순환
+                for (Integer score : targetMap.get(subject)) {
+
+                    if (isEssential) {
+                        System.out.println(i++ + "회차 등급 : " + essentialGrade(score));
+                    } else {
+                        System.out.println(i++ + "회차 등급 : " + optianalGrade(score));
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    // 필수 과목 등급 산출 메서드!
+    public String essentialGrade(int score) {
+        if (score >= 95) {
+            return "A";
+        } else if (score >= 90) {
+            return "B";
+        } else if (score >= 80) {
+            return "C";
+        } else if (score >= 70) {
+            return "D";
+        } else if (score >= 60) {
+            return "F";
+        } else {
+            return "N";
+        }
+    }
+
+    // 선택 과목 등급 산출 메서드!
+    public String optianalGrade(int score) {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80) {
+            return "B";
+        } else if (score >= 70) {
+            return "C";
+        } else if (score >= 60) {
+            return "D";
+        } else if (score >= 50) {
+            return "F";
+        } else {
+            return "N";
+        }
     }
 }
--- src/SpecificStudentAverage.java
+++ src/SpecificStudentAverage.java
@@ -3,6 +3,28 @@ import java.util.*;
 public class SpecificStudentAverage {
     public static ArrayList<Student> statusStudent = new ArrayList<Student>();
 
+    public static void gradeRegisterView() {
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            System.out.println("\n====== 점수 조회 ======");
+            System.out.println("1. 특정 학생 과목별 등급 조회 ");
+            System.out.println("2. 특정 학생 회차별 등급 조회 ");
+            System.out.println("3. 이전으로 돌아가기");
+            System.out.print("관리 항목을 선택하세요 : ");
+            int menuInput = sc.nextInt();
+
+            switch (menuInput) {
+                case 1 -> new GradeManagement().subjectGradeView();
+                case 2 -> new GradeManagement().studentRoundGradeView();
+                case 3 -> flag = false;
+                default -> {
+                    System.out.println("잘못된 입력입니다! \n처음으로 돌아갑니다.");
+                }
+            }
+        }
+    }
+}
 //    public static void averageCalculate() {
 //        List<Subject> subjectList = SubjectRepository.getSubjectStore(); // 모든 과목 리스트
 //
@@ -34,13 +56,6 @@ public class SpecificStudentAverage {
 //            }
 //        } // 상태 필터링
 //
-//        /*
-//        특정 상태 학생들 리스트 : SpecificStudentAverage.statusStudent
-//        특정 상태 학생들 총인원 : SpecificStudentAverage.statusStudent.size()
-//        과목 전체 리스트 : subjectList
-//        필수 과목 리스트 : SubjectStore.Subject.getIsEssential() == true
-//
-//        */
 //        for (Subject sub : subjectList) { // 모든 과목들 하나하나 꺼내는데,
 //            if (sub.getIsEssential() == true) { // 만약 필수 과목이라면
 //
@@ -48,61 +63,27 @@ public class SpecificStudentAverage {
 //        }
 //
 //
-//
-//
-//
-//
-//
-//        /*
-//
-//        while (flag) {
-//            System.out.println("조회할 과목의 이름을 써주세요");
-//            String subject = sc.next();
-//            for (String s : subjectList) {
-//                if (subject.equals(s)) {
-//                    sum = sumRoundScore(subject);
-//                    flag = false;
-//                    break;
-//                } else {
-//                    System.out.println("잘못 입력하셨습니다.\n");
-//                }
+//    public static void pickStatusUp(String status) {
+//        for (Student stu : StudentRepository.students) { // 학생들 리스트에서
+//            if (stu.getStatus().equalsIgnoreCase(status)) { // 학생의 상태를 갖고온게 파라미터 status와 같다면,
+//                statusStudent.add(stu); // 그 학생을 리스트에 넣는다
 //            }
-//        } // 과목 필터링
-//
-//        average = (double) sum / subjectList.size(); // 박용준 과목 평균
-//        // 문정석 과목 평균
-//
-//        */
-//
-//        System.out.printf("%s\n===============", status);
-//        while (학생수 > 0) {
-//            System.out.println("%s\n등급 : %c", 이름, 등급);
-//            System.out.println("===============");
-//            학생수 --;
 //        }
 //    }
-
-    public static void pickStatusUp(String status) {
-        for (Student stu : StudentRepository.students) { // 학생들 리스트에서
-            if (stu.getStatus().equalsIgnoreCase(status)) { // 학생의 상태를 갖고온게 파라미터 status와 같다면,
-                statusStudent.add(stu); // 그 학생을 리스트에 넣는다
-            }
-        }
-    }
-
-    // subjectScore10Round = student.getStudentMap
-    public static int sumRoundScore(String subjectName) { // 과목이름의 학생들 1~10회차 점수 총합
-        int sum = 0, totalSum = 0;
-        for (Student stu : statusStudent) {
-            // 학생 한 명의 subjectName 과목 1~10회차 점수
-            ArrayList<Integer> subjectScore10Round = stu.getStudentMap().get(SubjectRepository.getSubjectName(subjectName));
-            if (subjectScore10Round != null) {
-                for (Integer roundScore : subjectScore10Round) {
-                    sum += roundScore; // 1~10회차 점수 합산
-                    totalSum += sum;
-                }
-            }
-        }
-        return totalSum;
-    }
-}
+//
+//    // subjectScore10Round = student.getStudentMap
+//    public static int sumRoundScore(String subjectName) { // 과목이름의 학생들 1~10회차 점수 총합
+//        int sum = 0, totalSum = 0;
+//        for (Student stu : statusStudent) {
+//            // 학생 한 명의 subjectName 과목 1~10회차 점수
+//            ArrayList<Integer> subjectScore10Round = stu.getStudentMap().get(SubjectRepository.getSubjectName(subjectName));
+//            if (subjectScore10Round != null) {
+//                for (Integer roundScore : subjectScore10Round) {
+//                    sum += roundScore; // 1~10회차 점수 합산
+//                    totalSum += sum;
+//                }
+//            }
+//        }
+//        return totalSum;
+//    }
+//}
