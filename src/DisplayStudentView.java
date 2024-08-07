b1ce97555c19326e71e39bb0841566211ccf1f77
yeseo <ye_2002>
2024-08-07 22:21:08 +0900

    임시저장입니다.

--- src/DisplayStudentView.java
+++ src/DisplayStudentView.java
@@ -8,16 +8,16 @@ public class DisplayStudentView {
         boolean flag = true;
         while (flag) {
             System.out.println("\n======== 수강생 관리 ========");
-            System.out.println("1. 수강생 정보 등록");
-            System.out.println("2. 수강생 목록 조회");
-            System.out.println("00 : 이전으로 돌아가기");
+            System.out.println("1. 이전으로 돌아가기");
+            System.out.println("2. 수강생 정보 등록");
+            System.out.println("3. 수강생 목록 조회");
             System.out.print("메뉴를 선택해주세요 : ");
             int menuInput = sc.nextInt();
 
             switch (menuInput) {
-                case 1 -> StudentManagement.addStudent(); //수강생 정보 등록
-                case 2 -> StudentRepository.selectOption(); //수강생 조회
-                case 00 -> flag = false;
+                case 1 -> flag = false;
+                case 2 -> StudentManagement.addStudent(); //수강생 정보 등록
+                case 3 -> StudentRepository.selectOption(); //수강생 조회
                 default -> {
                     System.out.println("잘못된 입력입니다! \n처음으로 돌아갑니다.");
                 }
