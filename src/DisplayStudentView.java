1a155a9ad97eb974e8e96228384e0f7fed0f3cba
“cbb715” <iseul1121@gmail.com>
2024-08-07 11:23:37 +0900

    인터페이스 수정1

--- src/DisplayStudentView.java
+++ src/DisplayStudentView.java
@@ -11,7 +11,7 @@ public class DisplayStudentView {
             System.out.println("1. 수강생 정보 등록");
             System.out.println("2. 수강생 목록 조회");
             System.out.println("3. 이전으로 돌아가기");
-            System.out.println("관리 항복을 선택하세요 : ");
+            System.out.print("관리 항목을 선택하세요 : ");
             int menuInput = sc.nextInt();
 
             switch (menuInput) {
